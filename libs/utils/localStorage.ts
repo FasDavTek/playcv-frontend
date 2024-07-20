6bfb9036e2b9db3b741637163bc8ed3943b81240
TubiOb <Tubiobaloluwa@gmail.com>
2024-07-20 10:00:00 +0100

    domain deployment redirection

--- libs/utils/localStorage.ts
+++ libs/utils/localStorage.ts
@@ -9,7 +9,7 @@ export const AddToLocalStorage = (item: any, key: string) => {
   const data = GetItemsFromLocalStorage(key);
   // const joinedData = data ? [...data, item] : [item];
   // localStorage.setItem(key, JSON.stringify(joinedData));
-  const updatedData = [...data, item];
+  const updatedData = Array.isArray(data) ? [...data, item] : [item];
   localStorage.setItem(key, JSON.stringify(updatedData));
   // return joinedData;
 };
@@ -21,3 +21,33 @@ export const RemoveFromLocalStorage = (id: string, key: string) => {
   // localStorage.setItem(key, JSON.stringify(filteredData));
   localStorage.setItem(key, JSON.stringify(updatedData));
 };
+
+
+
+
+
+
+
+// export const GetItemsFromLocalStorage = (key: string) => {
+//   const data = localStorage.getItem(key);
+//   const parsedData = data ? JSON.parse(data) : null;
+//   return parsedData;
+// };
+
+// // REFACTOR:
+// export const AddToLocalStorage = (item: any, key: string) => {
+//   const data = GetItemsFromLocalStorage(key);
+//   // const joinedData = data ? [...data, item] : [item];
+//   // localStorage.setItem(key, JSON.stringify(joinedData));
+//   const updatedData = [...data, item];
+//   localStorage.setItem(key, JSON.stringify(updatedData));
+//   // return joinedData;
+// };
+
+// export const RemoveFromLocalStorage = (id: string, key: string) => {
+//   const data = GetItemsFromLocalStorage(key);
+//   // const filteredData = data ? data.filter((item: any) => item.id !== id) : [];
+//   const updatedData = data.filter((item: any) => item.id !== id);
+//   // localStorage.setItem(key, JSON.stringify(filteredData));
+//   localStorage.setItem(key, JSON.stringify(updatedData));
+// };
