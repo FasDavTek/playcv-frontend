ac95c138fb999eaede7ec5f2437af5f939d88967
TubiOb <Tubiobaloluwa@gmail.com>
2024-08-29 09:56:34 +0100

    added job management/vacancy page

--- libs/ui-components/Button/index.tsx
+++ libs/ui-components/Button/index.tsx
@@ -41,7 +41,7 @@ const Button: React.FC<ButtonProps> = ({
       type={type}
       className={`
       px-3 py-2 md:px-2 md:py-2 max-h-fit w-fit font-manrope 
-      !rounded-md font-semibold text-sm md:text-base  shadow-buttonShadow hover:shadow-lg hover:opacity-85 ${
+      !rounded-md font-light text-sm md:text-base  shadow-buttonShadow hover:shadow-lg hover:opacity-85 ${
         loading ? 'cursor-progress opacity-85' : 'cursor-pointer'
       } ${variants[variant]} ${className} ${
         icon || iconAfter || loading
