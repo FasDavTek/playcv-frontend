b872a48bd8e27dc556a1fa89ccc602038956caf1
TubiOb <Tubiobaloluwa@gmail.com>
2024-09-20 22:24:07 +0100

    sorted bug issues with functional components

--- apps/Employer/src/pages/JobBoard/index.tsx
+++ apps/Employer/src/pages/JobBoard/index.tsx
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom';
 import { Paper, Pagination, Box, Stack, Typography } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-import { Select, Radio } from '@video-cv/ui-components';
+import { Select, Radio, DatePicker } from '@video-cv/ui-components';
 import { useFilters } from '@video-cv/hooks';
 import { Images } from '@video-cv/assets';
+import { Controller, useForm } from 'react-hook-form';
+import dayjs from 'dayjs';
 
 const JobBoard = () => {
   const [jobs, setJobs] = useState([1, 2, 3, 4]);
+
+  const { control } = useForm();
 
   const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
     setJobs(e.target.value === 'all' ? [1, 2, 3, 4] : [3, 4]);
@@ -75,12 +79,17 @@ const JobBoard = () => {
             </p>
           </div>
           <div className="p-3 mx-auto flex flex-col gap-3">
-            <Select
-              options={[]}
-              label="Date Posted"
-              placeholder="Select Date"
-              containerClass="flex-1"
-            />
+            <Controller
+                name='datePosted'
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <DatePicker
+                    label="Date Posted"
+                    value={value}
+                    onChange={(newValue) => onChange(dayjs(newValue))}
+                  />
+                )}
+              />
 
             <Radio
               label="Job Status"
