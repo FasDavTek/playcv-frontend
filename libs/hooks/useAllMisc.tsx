2666b54c31960581de796d00ab9609369ff72ee8
TubiOb <Tubiobaloluwa@gmail.com>
2024-11-17 08:26:37 +0100

    completed vacancy flow

---
+++ libs/hooks/useAllMisc.tsx
@@ -0,0 +1,103 @@
+// import { useQuery, QueryFunction } from "@tanstack/react-query";
+// import { getData, postData } from '../utils/apis/apiMethods';
+// import CONFIG from '../utils/helpers/config';
+// import { apiEndpoints } from '../utils/apis/apiEndpoints';
+
+// interface MiscQueryParams {
+//     page?: number;
+//     limit?: number;
+//     download?: boolean;
+//     resource: string;
+// }
+
+// interface UseAllMiscResult<T> {
+//     data: T | null;
+//     isLoading: boolean;
+//     error: Error | null;
+// }
+
+// export const getAllMisc = async (params: MiscQueryParams) => {
+//     const queryParams = new URLSearchParams({
+//         ...(params.page !== undefined && { page: params.page.toString() }),
+//         ...(params.limit !== undefined && { limit: params.limit.toString() }),
+//         ...(params.download !== undefined && { download: params.download.toString() }),
+//         resource: params.resource,
+//     });
+
+    
+//     const data = await getData(`${CONFIG.BASE_URL}${apiEndpoints.GET_MISC}?${queryParams.toString()}`);
+//     return data;
+// };
+
+// export const useAllMisc = (params: MiscQueryParams) => {
+//     return useQuery({
+//         queryKey: ['AllMisc', params],
+//         queryFn: () => getAllMisc(params),
+//         refetchOnWindowFocus: false,
+//         retry: 1,
+//     });
+// };
+
+
+
+
+
+
+
+
+
+
+import { useState, useEffect } from 'react';
+import { getData } from '../utils/apis/apiMethods';
+import CONFIG from '../utils/helpers/config';
+import { apiEndpoints } from '../utils/apis/apiEndpoints';
+
+interface MiscQueryParams {
+    page?: number;
+    limit?: number;
+    download?: boolean;
+    resource: string;
+}
+
+interface Specialisation {
+    id: string;
+    name: string;
+}
+
+interface UseAllMiscResult {
+    specialisations: Specialisation[];
+    isLoading: boolean;
+    error: Error | null;
+}
+
+export const useAllMisc = (params: MiscQueryParams): UseAllMiscResult => {
+    const [specialisations, setSpecialisations] = useState<Specialisation[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<Error | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true);
+                const queryParams = new URLSearchParams({
+                    ...(params.page !== undefined && { page: params.page.toString() }),
+                    ...(params.limit !== undefined && { limit: params.limit.toString() }),
+                    ...(params.download !== undefined && { download: params.download.toString() }),
+                    resource: params.resource,
+                });
+
+                const url = `${CONFIG.BASE_URL}${apiEndpoints.GET_MISC}?${queryParams.toString()}`;
+                const result = await getData(url);
+                setSpecialisations(result.data || []);
+            } catch (err) {
+                setError(err instanceof Error ? err : new Error('An error occurred while fetching data'));
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [params.page, params.limit, params.download, params.resource]);
+
+    return { specialisations, isLoading, error };
+};
---
+++ libs/hooks/useAllState.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "react-query";
+import { getData, postData } from '../../libs/utils/apis/apiMethods';
+import CONFIG from '../../libs/utils/helpers/config';
+import { apiEndpoints } from '../../libs/utils/apis/apiEndpoints';
+
+export const getAllState = async () => {
+    const data = await getData(`${CONFIG.BASE_URL}${apiEndpoints.GET_MISC}`);
+    return data;
+};
+
+export const useAllState = () => {
+    return useQuery(['AllState'], getAllState, {
+        refetchOnWindowFocus: false, // Refetch on window focus
+        retry: 1,
+    });
+};
