bc22daf0f5ab05689d79e6c539ceff2a3ea8131f
TubiOb <Tubiobaloluwa@gmail.com>
2024-11-22 22:09:21 +0100

    sort errors

--- libs/utils/helpers/config.ts
+++ libs/utils/helpers/config.ts
@@ -1,16 +1,10 @@
 const baseUrl = import.meta.env.VITE_PUBLIC_BASEURL;
-const publicKey = import.meta.env.VITE_PUBLIC_KEY;
-const secretKey = import.meta.env.VITE_SECRET_KEY;
-const encryptKey = import.meta.env.VITE_ENCRYPT_KEY;
 const enquiry = import.meta.env.VITE_ENQUIRY_KEY;
 const paystackey = import.meta.env.VITE_PAYSTACK_API_KEY;
 const cloudflarekey = import.meta.env.VITE_CLOUDFLARE_API_KEY;
 
 const CONFIG = {
   BASE_URL: baseUrl,
-  PUBLIC_KEY: publicKey,
-  SECRET_KEY: secretKey,
-  ENCRYPTKEY: encryptKey,
   ENQUIRY: enquiry,
   PAYSTACK: paystackey,
   CLOUDFLARE: cloudflarekey,
