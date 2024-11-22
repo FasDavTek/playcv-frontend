46e399404e5ad68b5c83a0612224535348d386e3
TubiOb <Tubiobaloluwa@gmail.com>
2024-11-22 22:14:55 +0100

    sort errors

--- libs/utils/helpers/config.ts
+++ libs/utils/helpers/config.ts
@@ -1,11 +1,9 @@
 const baseUrl = import.meta.env.VITE_PUBLIC_BASEURL;
-const enquiry = import.meta.env.VITE_ENQUIRY_KEY;
 const paystackey = import.meta.env.VITE_PAYSTACK_API_KEY;
 const cloudflarekey = import.meta.env.VITE_CLOUDFLARE_API_KEY;
 
 const CONFIG = {
   BASE_URL: baseUrl,
-  ENQUIRY: enquiry,
   PAYSTACK: paystackey,
   CLOUDFLARE: cloudflarekey,
 };
