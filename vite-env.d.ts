18cd80f8bd050b4da25c4585aac50615cb81b78b
TubiOb <Tubiobaloluwa@gmail.com>
2024-11-22 21:57:50 +0100

    sorting production errors

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
     readonly VITE_PUBLIC_BASEURL: string
     readonly VITE_PUBLIC_KEY: string
     readonly VITE_PAYSTACK_API_KEY: string
+    readonly VITE_CLOUDFLARE_API_KEY: string
     readonly VITE_FLUT_URL: string
     readonly VITE_SECRET_KEY: string
     readonly VITE_ENCRYPT_KEY: string
