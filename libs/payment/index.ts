1238a5c46c95eb362ab68ead0a6dda2901450cfb
TubiOb <Tubiobaloluwa@gmail.com>
2024-12-16 19:28:38 +0100

    updating bug issues

--- libs/payment/index.ts
+++ libs/payment/index.ts
@@ -1,6 +1,2 @@
 export { default as usePaystack } from './usePaystack';
-<<<<<<< HEAD
 export type { PaymentDetails } from './usePaystack';
-=======
-export type { PaymentDetails } from './usePaystack';
->>>>>>> bb7953228a3b4517bf401166d99b77c2680a2274
