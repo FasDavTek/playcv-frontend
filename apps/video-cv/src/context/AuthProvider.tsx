ef1180ec623ec5611d39580c66a73932b7363ac6
TubiOb <Tubiobaloluwa@gmail.com>
2025-02-08 17:22:12 +0100

    updates so far, based on feebdacks

--- apps/video-cv/src/context/AuthProvider.tsx
+++ apps/video-cv/src/context/AuthProvider.tsx
@@ -2,36 +2,21 @@ import { useContext, createContext, ReactNode, useState, useEffect, } from 'reac
 
 import { GetItemsFromLocalStorage, RemoveFromLocalStorage, AddToLocalStorage, } from '@video-cv/utils';
 
-interface AuthState {
-  isAuthenticated: boolean
-  user: any // Replace 'any' with a more specific user type if available
-}
-
-interface AuthContextType {
-  authState: AuthState
-  handleLogin: (userData: any, userType: string) => void // Replace 'any' with a more specific user data type
-  handleLogout: () => void
-}
-
-export const AuthContext = createContext<AuthContextType | null>(null); // Specify the context type
+export const AuthContext = createContext<any>(null); // Specify the context type
 
 const AUTH_LOCALSTORAGE_KEY = 'VIDEO-CV-AUTH';
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
   // TODO: Make this a useReducer
-  // const [authState, setAuthState] = useState(
-  //   GetItemsFromLocalStorage(AUTH_LOCALSTORAGE_KEY) ?? null
-  // );
-
-  const [authState, setAuthState] = useState<AuthState>(() => {
+  const [authState, setAuthState] = useState(() => {
     const storedAuth = GetItemsFromLocalStorage(AUTH_LOCALSTORAGE_KEY)
     return storedAuth ? { isAuthenticated: true, user: storedAuth } : { isAuthenticated: false, user: null }
-  })
+});
 
-  const handleLogin = (userData: any, userType: string) => {
-    const userAuthData = { isAuthenticated: true, userType, userData };
+  const handleLogin = (userType: string) => {
+    const userAuthData = { isAuthenticated: true, userType };
     localStorage.setItem(AUTH_LOCALSTORAGE_KEY, JSON.stringify(userAuthData));
-    setAuthState({ isAuthenticated: true, user: userAuthData })
+    setAuthState({isAuthenticated: true, user: userAuthData});
   };
 
   const handleLogout = () => {
@@ -55,3 +40,100 @@ export default AuthProvider;
 export const useAuth = () => {
   return useContext(AuthContext);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useContext, createContext, ReactNode, useState, useEffect, } from 'react';
+
+// import { GetItemsFromLocalStorage, RemoveFromLocalStorage, AddToLocalStorage, } from '@video-cv/utils';
+
+// interface AuthState {
+//   isAuthenticated: boolean
+//   user: any // Replace 'any' with a more specific user type if available
+// }
+
+// interface AuthContextType {
+//   authState: AuthState
+//   handleLogin: (userData: any, userType: string) => void // Replace 'any' with a more specific user data type
+//   handleLogout: () => void
+// }
+
+// export const AuthContext = createContext<AuthContextType | null>(null); // Specify the context type
+
+// const AUTH_LOCALSTORAGE_KEY = 'VIDEO-CV-AUTH';
+
+// const AuthProvider = ({ children }: { children: ReactNode }) => {
+//   // TODO: Make this a useReducer
+//   // const [authState, setAuthState] = useState(
+//   //   GetItemsFromLocalStorage(AUTH_LOCALSTORAGE_KEY) ?? null
+//   // );
+
+//   const [authState, setAuthState] = useState<AuthState>(() => {
+//     const storedAuth = GetItemsFromLocalStorage(AUTH_LOCALSTORAGE_KEY)
+//     return storedAuth ? { isAuthenticated: true, user: storedAuth } : { isAuthenticated: false, user: null }
+//   })
+
+//   const handleLogin = (userData: any, userType: string) => {
+//     const userAuthData = { isAuthenticated: true, userType, userData };
+//     localStorage.setItem(AUTH_LOCALSTORAGE_KEY, JSON.stringify(userAuthData));
+//     setAuthState({ isAuthenticated: true, user: userAuthData })
+//   };
+
+//   const handleLogout = () => {
+//     localStorage.removeItem(AUTH_LOCALSTORAGE_KEY)
+//     setAuthState({ isAuthenticated: false, user: null })
+//   };
+
+//   // useEffect(() => {
+
+//   // }, [])
+
+//   return (
+//     <AuthContext.Provider value={{ authState, handleLogin, handleLogout }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export default AuthProvider;
+
+// export const useAuth = () => {
+//   return useContext(AuthContext);
+// };
--- libs/utils/localStorage.ts
+++ libs/utils/localStorage.ts
@@ -30,13 +30,15 @@
 
 import type { ICartItem } from './../../apps/video-cv/src/context/CartProvider';
 
-export const GetItemsFromLocalStorage = (key: string): ICartItem[] => {
+export const GetItemsFromLocalStorage = (key: any) => {
   const data = localStorage.getItem(key)
   return data ? JSON.parse(data) : []
 }
 
-export const AddToLocalStorage = (items: ICartItem[], key: string): void => {
-  localStorage.setItem(key, JSON.stringify(items))
+export const AddToLocalStorage = (items: any, key: string): void => {
+  const data = GetItemsFromLocalStorage(key);
+  const updatedData = Array.isArray(data) ? [...data, items] : [items];
+  localStorage.setItem(key, JSON.stringify(updatedData))
 }
 
 export const RemoveFromLocalStorage = (id: string, key: string): void => {
