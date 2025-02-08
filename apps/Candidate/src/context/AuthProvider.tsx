6053d794cd69cc64beb153679965a42c2b0ef2e1
TubiOb <Tubiobaloluwa@gmail.com>
2025-02-08 17:26:29 +0100

    updates so far, based on feebdacks

--- apps/Candidate/src/context/AuthProvider.tsx
+++ apps/Candidate/src/context/AuthProvider.tsx
@@ -64,6 +64,57 @@
 
 
 
+import { useContext, createContext, ReactNode, useState, useEffect, } from 'react';
+
+import { GetItemsFromLocalStorage, RemoveFromLocalStorage, AddToLocalStorage, } from '@video-cv/utils';
+
+export const AuthContext = createContext<any>(null); // Specify the context type
+
+const AUTH_LOCALSTORAGE_KEY = 'VIDEO-CV-AUTH';
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  // TODO: Make this a useReducer
+  const [authState, setAuthState] = useState(() => {
+    const storedAuth = GetItemsFromLocalStorage(AUTH_LOCALSTORAGE_KEY)
+    return storedAuth ? { isAuthenticated: true, user: storedAuth } : { isAuthenticated: false, user: null }
+});
+
+  const handleLogin = (userType: string) => {
+    const userAuthData = { isAuthenticated: true, userType };
+    localStorage.setItem(AUTH_LOCALSTORAGE_KEY, JSON.stringify(userAuthData));
+    setAuthState({isAuthenticated: true, user: userAuthData});
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem(AUTH_LOCALSTORAGE_KEY)
+    setAuthState({ isAuthenticated: false, user: null })
+  };
+
+  // useEffect(() => {
+
+  // }, [])
+
+  return (
+    <AuthContext.Provider value={{ authState, handleLogin, handleLogout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider")
+  }
+  return context
+};
+
+
+
+
+
 
 
 
